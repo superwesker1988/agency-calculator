e337f2dab777e3bc6a2654c9dcc59cad9010bb8c
Ken Qin <ken.qin@cerner.com>
2018-08-28 09:30:23 -0500

    Fixed typo.

--- src/core/utilities.js
+++ src/core/utilities.js
@@ -1,22 +1,22 @@
 /**
  * Returns the total commission fee for given face amout based on agent hirearchy and commission plan.
  * @param {number} faceAmount - The face amount of sale
- * @param {AgentHierarchy} agentHirearchy - The agent hirearchy used in the transaction
+ * @param {AgentHierarchy} agentHierarchy - The agent hirearchy used in the transaction
  * @param {CommissionPlan} plan - The commission plan used in the transaction
  * @returns {number} The total commission fee for given face amout based on agent hirearchy and commission plan.
  */
-const getTotalCommissionFee = (faceAmount = 0, agentHirearchy = null, plan = null) => {
+const getTotalCommissionFee = (faceAmount = 0, agentHierarchy = null, plan = null) => {
     if (!faceAmount) {
         return 0;
     }
-    if (!agentHirearchy || !plan) {
+    if (!agentHierarchy || !plan) {
         return 0;
     }
     let totalCommission = 0;
     let agentIndex = 0;
     for (let tierIndex = 0; tierIndex < plan.tierRate.length; tierIndex++) {
-        if (agentIndex < agentHirearchy.agents.length) {
-            totalCommission += faceAmount * plan.tierRate[tierIndex] * agentHirearchy.agents[agentIndex++].rate;
+        if (agentIndex < agentHierarchy.agents.length) {
+            totalCommission += faceAmount * plan.tierRate[tierIndex] * agentHierarchy.agents[agentIndex++].rate;
         } else {
             break;
         }
